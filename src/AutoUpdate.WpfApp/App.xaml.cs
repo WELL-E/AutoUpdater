707fbd27e37f6e3b30a5419f21d068e0d7b98206
朱震 <zhuzhen723723@outlook.com>
2020-09-04 00:57:43 +0800

    修复：不能在同一 AppDomain 中创建多个 System.Windows.Application

--- src/AutoUpdate.WpfApp/App.xaml.cs
+++ src/AutoUpdate.WpfApp/App.xaml.cs
@@ -13,54 +13,8 @@ namespace GeneralUpdate.WpfApp
     /// <summary>
     /// App.xaml 的交互逻辑
     /// </summary>
-    public partial class App : Application, ISingleInstanceApp
+    public partial class App : Application
     {
-        private const string AppId = "{7F280539-0814-4F9C-95BF-D2BB60023657}";
-
-        [STAThread]
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            string[] resultArgs = null;
-
-            if (e.Args == null || e.Args.Length == 0)
-            {
-                resultArgs = new string[6] {
-                "0.0.0.0",
-                "1.1.1.1",
-                "https://github.com/WELL-E",
-                 "http://192.168.50.225:7000/update.zip",
-                 @"E:\PlatformPath",
-                "509f0ede227de4a662763a4abe3d8470",
-                 };
-            }
-            else
-            {
-                resultArgs = e.Args;
-            }
-
-            if (resultArgs.Length != 6) return;
-            if (SingleInstance<App>.InitializeAsFirstInstance(AppId))
-            {
-                var win = new MainWindow();
-                var vm = new MainViewModel(resultArgs, win.Close);
-                win.DataContext = vm;
-
-                var application = new App();
-                application.InitializeComponent();
-                application.Run(win);
-                SingleInstance<App>.Cleanup();
-            }
-        }
-
-        public bool SignalExternalCommandLineArgs(IList<string> args)
-        {
-            if (this.MainWindow.WindowState == WindowState.Minimized)
-            {
-                this.MainWindow.WindowState = WindowState.Normal;
-            }
-            this.MainWindow.Activate();
-
-            return true;
-        }
+       
     }
 }
---
+++ src/AutoUpdate.WpfApp/Program.cs
@@ -0,0 +1,45 @@
+﻿using AutoUpdate.WpfApp.ViewModels;
+using GeneralUpdate.Core.Utils;
+using GeneralUpdate.WpfApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AutoUpdate.WpfApp
+{
+    class Program : Application, ISingleInstanceApp
+    {
+        private const string AppId = "{7F280539-0814-4F9C-95BF-D2BB60023657}";
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            //if (args.Length != 6) return; 
+            if (SingleInstance<Program>.InitializeAsFirstInstance(AppId))
+            {
+                var win = new MainWindow();
+                var vm = new MainViewModel(args, win.Close);
+                win.DataContext = vm;
+
+                var application = new Program();
+                //application.InitializeComponent();
+                application.Run(win);
+                SingleInstance<Program>.Cleanup();
+            }
+        }
+
+        public bool SignalExternalCommandLineArgs(IList<string> args)
+        {
+            if (this.MainWindow.WindowState == WindowState.Minimized)
+            {
+                this.MainWindow.WindowState = WindowState.Normal;
+            }
+            this.MainWindow.Activate();
+
+            return true;
+        }
+    }
+}
