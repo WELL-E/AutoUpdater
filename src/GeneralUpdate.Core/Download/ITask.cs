50e9365e1727e896289f73f700f4dd24c7716daf
callmejust <ZHUZHEN723723@163.COM>
2022-01-07 23:55:24 +0800

    重构多任务下载逻辑设计

---
+++ src/GeneralUpdate.Core/Download/ITask.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeneralUpdate.Core.Download
+{
+    internal interface ITask
+    {
+        bool IsCompleted { get; }
+
+    }
+}
