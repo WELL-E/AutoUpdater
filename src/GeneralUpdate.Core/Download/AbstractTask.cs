ad0c4c63e86b41bec6dbe5f03c95a60994d02cc0
JusterZhu <zhuzhen723723@outlook.com>
2022-01-12 23:42:29 +0800

    添加task

--- src/GeneralUpdate.Core/Download/AbstractTask.cs
+++ src/GeneralUpdate.Core/Download/AbstractTask.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace GeneralUpdate.Core.Download
 {
-    internal abstract class AbstractTask<T> : IProgress<T> where T : class
+    internal abstract class AbstractTask<T> : INotifyCompletion , IProgress<T> where T : class
     {
+        public void OnCompleted(Action continuation)
+        {
+            throw new NotImplementedException();
+        }
+
         public void Report(T value)
         {
             throw new NotImplementedException();
--- src/GeneralUpdate.Core/Download/AbstractTaskManager.cs
+++ src/GeneralUpdate.Core/Download/AbstractTaskManager.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace GeneralUpdate.Core.Download
 {
-    internal class AbstractTaskManager
+    internal class AbstractTaskManager : INotifyCompletion
     {
+        public void OnCompleted(Action continuation)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
