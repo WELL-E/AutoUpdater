2e00c6a49d62b0e147b0f65976f466bb6633382a
JusterZhu <zhuzhen723723@outlook.com>
2022-01-12 23:56:05 +0800

    添加Download实例

---
+++ src/GeneralUpdate.Core/Download/DownloadManager.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeneralUpdate.Core.Download
+{
+    internal class DownloadManager : AbstractTaskManager
+    {
+    }
+}
---
+++ src/GeneralUpdate.Core/Download/DownloadTask.cs
@@ -0,0 +1,11 @@
+﻿using GeneralUpdate.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeneralUpdate.Core.Download
+{
+    internal class DownloadTask : AbstractTask<UpdateVersion>
+    {
+    }
+}
