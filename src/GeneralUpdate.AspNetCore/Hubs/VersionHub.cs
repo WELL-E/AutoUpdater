f47278229acfc8c3fef0d0e20f50c48620abae9b
callmejust <ZHUZHEN723723@163.COM>
2022-02-15 23:58:57 +0800

    添加SignalR基础内容

--- src/GeneralUpdate.AspNetCore/Hubs/VersionHub.cs
+++ src/GeneralUpdate.AspNetCore/Hubs/VersionHub.cs
@@ -1,10 +1,29 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace GeneralUpdate.AspNetCore.Hubs
 {
-    internal class VersionHub
+    internal class VersionHub : Hub
     {
+        //TODO: SignalR
+
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public async Task Login(string name)
+        {
+            await Clients.AllExcept(Context.ConnectionId).SendAsync("online", $"{ name }in the group.");
+            await Clients.Group("JusterGroup").SendAsync("online", $"{ name }in the group.");
+        }
+
+        public async Task SignOut(string name)
+        {
+            await Clients.AllExcept(Context.ConnectionId).SendAsync("online", $"{ name }leave the group.");
+        }
     }
 }
