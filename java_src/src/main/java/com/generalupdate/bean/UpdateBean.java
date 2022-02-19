08b5117b478b5a51df3ecd77a58cfede693b36b3
Juster.zhu <zhuzhen723723@163.com>
2022-02-20 02:29:58 +0800

    添加java bean

--- java_src/src/main/java/com/generalupdate/bean/UpdateBean.java
+++ java_src/src/main/java/com/generalupdate/bean/UpdateBean.java
@@ -1,4 +1,24 @@
 package com.generalupdate.bean;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+
+import java.util.List;
+
+@Configuration
 public class UpdateBean {
+
+    @Bean
+    @Scope(value = "singleton" )
+    public List<String> validate(){
+        return null;
+    }
+
+    @Bean
+    @Scope(value = "singleton")
+    public List<String> version(){
+        return null;
+    }
 }
--- java_src/src/main/java/com/generalupdate/controller/UpdateController.java
+++ java_src/src/main/java/com/generalupdate/controller/UpdateController.java
@@ -1,10 +1,15 @@
 package com.generalupdate.controller;
 
+import com.generalupdate.bean.UpdateBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/update")
 public class UpdateController {
+
+    @Autowired
+    UpdateBean updateBean;
 
     @PostMapping("validate")
     public String validate(@RequestBody Integer clientType,@RequestBody String clientVersion){
