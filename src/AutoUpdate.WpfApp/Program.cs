d273579c6fb0b1382666dfe883987139555fc008
callmejust <ZHUZHEN723723@163.COM>
2022-02-23 23:31:01 +0800

    删除不必要项目

--- src/AutoUpdate.WpfApp/Program.cs
+++ src/AutoUpdate.WpfApp/Program.cs
@@ -1,57 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Windows;
+﻿//class Program : Application, ISingleInstanceApp
+//{
+//    private const string AppId = "{7F280539-0814-4F9C-95BF-D2BB60023657}";
 
-namespace AutoUpdate.WpfApp
-{
-    class Program : Application//, ISingleInstanceApp
-    {
-        private const string AppId = "{7F280539-0814-4F9C-95BF-D2BB60023657}";
+//    [STAThread]
+//    static void Main(string[] args)
+//    {
+//        //ISingleInstanceApp Can only be used in the framework of .net framework .
+//        string[] resultArgs = null;
 
-        [STAThread]
-        static void Main(string[] args)
-        {
-            //ISingleInstanceApp Can only be used in the framework of .net framework .
-            string[] resultArgs = null;
+//        if (args == null || args.Length == 0)
+//        {
+//            resultArgs = new string[6] {
+//            "0.0.0.0",
+//            "1.1.1.1",
+//            "https://github.com/WELL-E",
+//             "http://192.168.50.225:7000/update.zip",
+//             @"E:\PlatformPath",
+//            "371ada8087cf7651eafaadf4fcc214a5",
+//             };
+//        }
+//        else
+//        {
+//            resultArgs = args;
+//        }
 
-            if (args == null || args.Length == 0)
-            {
-                resultArgs = new string[6] {
-                "0.0.0.0",
-                "1.1.1.1",
-                "https://github.com/WELL-E",
-                 "http://192.168.50.225:7000/update.zip",
-                 @"E:\PlatformPath",
-                "371ada8087cf7651eafaadf4fcc214a5",
-                 };
-            }
-            else
-            {
-                resultArgs = args;
-            }
+//        if (resultArgs.Length != 6) return;
+//        if (SingleInstance<Program>.InitializeAsFirstInstance(AppId))
+//        {
+//            var win = new MainWindow();
+//            var vm = new MainViewModel(resultArgs, win.Close);
+//            win.DataContext = vm;
 
-            if (resultArgs.Length != 6) return;
-            //if (SingleInstance<Program>.InitializeAsFirstInstance(AppId))
-            //{
-            //    var win = new MainWindow();
-            //    var vm = new MainViewModel(resultArgs, win.Close);
-            //    win.DataContext = vm;
+//            var application = new Program();
+//            application.Run(win);
+//            SingleInstance<Program>.Cleanup();
+//        }
+//    }
 
-            //    var application = new Program();
-            //    application.Run(win);
-            //    SingleInstance<Program>.Cleanup();
-            //}
-        }
+//    public bool SignalExternalCommandLineArgs(IList<string> args)
+//    {
+//        if (this.MainWindow.WindowState == WindowState.Minimized)
+//        {
+//            this.MainWindow.WindowState = WindowState.Normal;
+//        }
+//        this.MainWindow.Activate();
 
-        public bool SignalExternalCommandLineArgs(IList<string> args)
-        {
-            if (this.MainWindow.WindowState == WindowState.Minimized)
-            {
-                this.MainWindow.WindowState = WindowState.Normal;
-            }
-            this.MainWindow.Activate();
-
-            return true;
-        }
-    }
-}
+//        return true;
+//    }
+//}
