3a8d058a62b43a6a229b9dc9481760abac31c95e
callmejust <ZHUZHEN723723@163.COM>
2022-02-28 22:46:40 +0800

    更新MAUI框架版本

---
+++ src/AutoUpdate.MAUI/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace AutoUpdate.MAUI
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            this.DisplayAlert("title","hello,world.","ok");
+        }
+    }
+}
