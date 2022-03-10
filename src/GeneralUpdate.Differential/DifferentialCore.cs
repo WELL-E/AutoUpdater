c2af004b90d258bf7a0c4f3d6affb67d4a136bd5
zhuzhen723723@163.com <zhuzhen723723@163.com>
2022-03-10 20:50:57 +0800

    完善增量更新

--- src/GeneralUpdate.Differential/DifferentialCore.cs
+++ src/GeneralUpdate.Differential/DifferentialCore.cs
@@ -1,13 +1,14 @@
 ﻿using GeneralUpdate.Differential.BinaryFile;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace GeneralUpdate.Differential
 {
     public class DifferentialCore
     {
-        public async Task<bool> Launch(byte[] oldfileArrary, byte[] newfileArray)
+        private async Task<bool> Comparebytes(byte[] oldfileArrary, byte[] newfileArray)
         {
             return await Task.Run(() =>
             {
@@ -18,7 +19,42 @@ namespace GeneralUpdate.Differential
             });
         }
 
-        public void Generate()
-        { }
+        /// <summary>
+        /// Generate diff file.
+        /// </summary>
+        /// <param name="sourcePath"></param>
+        /// <param name="targetPath"></param>
+        public async Task Generate(string sourcePath,string targetPath)
+        {
+            List<string> sourceFiles = new List<string>();
+            List<string> targetFiles = new List<string>();
+            Find(sourcePath, ref sourceFiles);
+            Find(targetPath, ref targetFiles);
+            foreach (var item in sourceFiles)
+            {
+                var sourceFileBytes = File.ReadAllBytes(item);
+                var targetFileBytes = File.ReadAllBytes(item);
+                await Comparebytes(sourceFileBytes, targetFileBytes);
+            }
+        }
+
+        /// <summary>
+        /// Find matching files recursively.
+        /// </summary>
+        /// <param name="directory">root directory</param>
+        /// <param name="files">result file list</param>
+        private void Find(string rootDirectory, ref List<string> files)
+        {
+            var rootDirectoryInfo = new DirectoryInfo(rootDirectory);
+            foreach (var file in rootDirectoryInfo.GetFiles())
+            {
+                var fullName = file.FullName;
+                files.Add(fullName);
+            }
+            foreach (var dir in rootDirectoryInfo.GetDirectories())
+            {
+                Find(dir.FullName, ref files);
+            }
+        }
     }
 }
