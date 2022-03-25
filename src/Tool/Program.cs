5547e397be419443ab52b45cbc063124de434a6e
zhuzhen723723@163.com <zhuzhen723723@163.com>
2022-03-25 23:54:21 +0800

    添加打包工具工程

---
+++ src/Tool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tool
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
